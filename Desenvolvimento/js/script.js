21d044019a3e21c0f3589c7ef8e408ca6d48c1ef
CamilaAlmeida07 <camilaalveslima07@gmail.com>
2016-10-20 12:23:48 -0200

    Alteração no menu
    menu alterado

--- Desenvolvimento/js/script.js
+++ Desenvolvimento/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+
+	var flag = false;
+
+	$("#menu").hover(function(){
+		$("#nav li>a>p").delay(800).css('visibility', 'visible');
+		flag = true;
+	});
+	$("#menu").mouseout(function(){
+		$('#nav li>a>p').delay(800).css('visibility', 'hidden');
+	});
+	
 	$('#nav li>a').click(function(){
 		$('.selected').removeClass('selected');
 		$(this).addClass('selected');
